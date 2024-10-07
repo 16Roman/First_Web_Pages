4ec3c6fc8ec3d632daa10a9f2ae85939620e1b78
Roman Giannuzzi <165339005+16Roman@users.noreply.github.com>
2024-10-07 16:59:57 -0400

    Add files via upload

---
+++ Portfolio/Lab7/script.js
@@ -0,0 +1,10 @@
+function toggleMenu(arg){
+    if(document.getElementById(arg).style.display=='block'){
+        document.getElementById(arg).style.display='none';
+    }
+    else{
+        document.getElementById(arg).style.display='block';
+    }
+    
+
+}
